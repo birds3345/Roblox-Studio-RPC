1decde4be9dba74b473d33e259f58dcfe314733a
Viken <0xzolemail@gmail.com>
2025-04-08 11:38:09 -0400

    Better logic for detecting the current active script

--- plugin/src/Main.server.lua
+++ plugin/src/Main.server.lua
@@ -4,7 +4,7 @@ local HttpService = game:GetService("HttpService")
 
 local MarketplaceService = game:GetService("MarketplaceService")
 local Selection = game:GetService("Selection")
-
+local StudioService = game:GetService("StudioService")
 local RunService = game:GetService("RunService")
 
 local States = require(script.Parent.States)
@@ -64,45 +64,17 @@ if not RunService:IsEdit() then
 end
 
 
---store them as a string to avoid edge cases for ScriptEditorService.TextDocumentDidClose
-local documents: {string} = {}
-
-
-for i,v in ScriptEditorService:GetScriptDocuments() do
-	if v:GetScript() then
-		table.insert(documents, v:GetScript():GetFullName())
-	end
-end
-
-ScriptEditorService.TextDocumentDidOpen:Connect(function(document: ScriptDocument): ()
-	if not document:GetScript() then return end
-	
-	table.insert(documents, document:GetScript():GetFullName())
-	
-	set(States.Scripting, document:GetScript():GetFullName())
-end)
-
-ScriptEditorService.TextDocumentDidClose:Connect(function(document: ScriptDocument): ()
-	local found = table.find(documents, document.Name)
-	if not found then return end
-	
-	table.remove(documents, found)
-	
-	if currentState ~= States.Scripting then return end
-	
-	if #documents > 0 then
-		set(States.Scripting, documents[#documents])
-		
+local function activeScriptChanged()
+	if StudioService.ActiveScript then
+		set(States.Scripting, StudioService.ActiveScript:GetFullName())
 	else
 		set(States.Idle)
 	end
-end)
+end
 
-ScriptEditorService.TextDocumentDidChange:Connect(function(document: ScriptDocument)
-	if not document:GetScript() then return end
-	
-	set(States.Scripting, document:GetScript():GetFullName())
-end)
+activeScriptChanged()
+StudioService:GetPropertyChangedSignal("ActiveScript"):Connect(activeScriptChanged)
+
 
 Selection.SelectionChanged:Connect(function()
 	local hasPart = false
